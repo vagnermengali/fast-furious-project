a0ae631a7bf6344b03bfd7e946c1ec757b3b6f92
vagnermengali <vagmengali2015@gmail.com>
2025-01-27 16:50:23 -0300

    feat: subindo deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/fast-furious-project",
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
    },
